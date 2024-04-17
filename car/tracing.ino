808c39076b8779ff20487c71393e84df54c80a2d
Sam-Wu-911117 <h4572306@gmail.com>
2024-04-17 21:27:45 +0800

    car turn judging modify

--- car/tracing.ino
+++ car/tracing.ino
@@ -40,7 +40,7 @@ void tracing() {
    else if (data[0]==0 && data[1]==0 && data[2]==0 && data[3]==1 && data[4]==1){  //左邊檢測到黑線
     TurnLeft();
   }
-  else if (data[0]==1 && data[1]==1 && data[2]==1 && data[3]==0 && data[4]==0) {  //右邊檢測到黑線
+  else if (data[0]==1 && data[1]==1 && data[2]==0 && data[3]==0 && data[4]==0) {  //右邊檢測到黑線
     TurnRight();
   }
   // else if (data[0]==0 && data[1]==0 && data[2]==0 && data[3]==1 && data[4]==1) {  //左邊檢測到黑線
