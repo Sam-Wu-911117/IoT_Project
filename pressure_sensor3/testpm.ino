1e125d97509fb0ff05cbb4fd42935d38d768eab5
Sam-Wu-911117 <h4572306@gmail.com>
2024-04-22 19:29:40 +0800

    rewrite pressure sensor comparing

--- pressure_sensor3/testpm.ino
+++ pressure_sensor3/testpm.ino
@@ -1,7 +1,6 @@
-/*const int sensorPin[] = {A0, A1};  // 感測器引腳
-const int numSensors = 2;  // 感測器數量
-
-int sensorValues[numSensors];  // 用於存儲感測器值的數組
+const int PressurePin[4] = {A0, A1,A2,A3};
+const int numSensors = 4;
+int sensorValues[numSensors]; 
 
 // 定義壓力感測器的量程 (0.1kg ~ 10kg)
 const float minPressure = 0.1; // 最小壓力（kg）
@@ -9,120 +8,38 @@ const float maxPressure = 10.0; // 最大壓力（kg）
 
 void setup() {
   Serial.begin(9600);
+  pinMode(PressurePin[0],INPUT);
+  pinMode(PressurePin[1],INPUT);
 }
 
 void loop() {
-  for (int i = 0; i < numSensors; i++) {
-    sensorValues[i] = analogRead(sensorPin[i]);
-    delay(10);
-  }
-
-  for (int i = 0; i < numSensors; i++) {
-    float pressure = map(sensorValues[i], 0, 1023, minPressure, maxPressure);
-
-    // 找出最大的感測器引腳
-    int maxPin;
-    if (A0 > A1 && A0 > A2 ) {
-      maxPin = A0;
-    }
-    else if (A1 > A0 && A1 > A2 ) {
-      maxPin = A1;
-    }
-    /*if(A2>A0 && A2>A1 && A2>A3){
-      maxPin=A2;
-      }
-      if(A3>A0 && A3>A1 && A3>A2){
-      maxPin=A3;
-      }
-
-    if (pressure <= 93) {
-      float percentage = pressure * (100 / 93);
-      //Serial.print("感測器 ");
-      //Serial.print(maxPin);
-      //Serial.print("Pressure Percentage: ");
-      //Serial.print(percentage);
-      //Serial.println("%");
-
-      Serial.print("最大感測器 ");
-      Serial.println(maxPin);
-      Serial.print("Pressure Percentage: ");
-      Serial.print(percentage);
-      Serial.println("%");
-
-    }
-    else
-    {
-      Serial.println("Pressure out of range!");
-    }
-    delay(1000);
-  }
-}*/
-
-
-
-//2
-const int pressureSensorPin1 = A0; // 感測器引腳
-const int pressureSensorPin2 = A1; // 感測器引腳
-
-const int numSensors = 2;  // 感測器數量
-
-int sensorValues[numSensors];  // 用於存儲感測器值的數組
-
-// 定義壓力感測器的量程 (0.1kg ~ 10kg)
-const float minPressure = 0.1; // 最小壓力（kg）
-const float maxPressure = 10.0; // 最大壓力（kg）
-int sensorValue1=0;
-int sensorValue2=0;
-float pressure1=0;
-float pressure2=0;
-void setup() {
-  Serial.begin(9600);
-  pinMode(pressureSensorPin1,INPUT);
-  pinMode(pressureSensorPin2,INPUT);
-}
-
-void loop() {
-  sensorValue1= analogRead(pressureSensorPin1);
-  delay(10);
-  sensorValue2 = analogRead(pressureSensorPin2);
   
-  pressure1=map(sensorValues1, 0, 1023, minPressure, maxPressure);//PR=0~1023-->Kg=0.1~10
-
   for (int i = 0; i < numSensors; i++) {
-    float pressure[i] = map(sensorValues[i], 0, 1023, minPressure, maxPressure);//PR=0~1023-->Kg=0.1~10
-
-    // 找出最大的感測器引腳
-
-    if (pressure[0] > pressure[1] ) {
-      sensorPin[0];
-    }
-    else if (pressure[1] > pressure[0] ) {
-      sensorPin[1];
-    }
-    /*if(A2>A0 && A2>A1 && A2>A3){
-      maxPin=A2;
-      }
-      if(A3>A0 && A3>A1 && A3>A2){
-      maxPin=A3;
-      }*/
-
-    if (pressure <= 93) {
-      float percentage = pressure * (100 / 93);
-      //Serial.print("感測器 ");
-      //Serial.print(maxPin);
-      //Serial.print("Pressure Percentage: ");
-      //Serial.print(percentage);
-      //Serial.println("%");
-
-      Serial.print("最大感測器 ");
-      Serial.println(maxPin);
-      Serial.print("Pressure Percentage: ");
+    
+    float pressure = map(sensorValues[i], 0, 1023, minPressure, maxPressure);
+    sensorValues[i] = analogRead(PressurePin[i]);
+    if (pressure<=93){
+      
+      float percentage=pressure*(100/93);
+      Serial.print("Pressure ");
+      Serial.print(i);
+      Serial.print(" Percentage: ");
       Serial.print(percentage);
       Serial.println("%");
-
+      if(sensorValues[0]>sensorValues[1] && sensorValues[0]>sensorValues[2] && sensorValues[0]>sensorValues[3]){
+        Serial.println("maxpin : A0");
+      }
+      else if(sensorValues[1]>sensorValues[0]  && sensorValues[1]>sensorValues[2] && sensorValues[1]>sensorValues[3]){
+        Serial.println("maxpin : A1");
+      }
+      else if(sensorValues[2]>sensorValues[0]  && sensorValues[2]>sensorValues[1] && sensorValues[2]>sensorValues[3]){
+        Serial.println("maxpin : A2");
+      }
+      else if(sensorValues[3]>sensorValues[0]  && sensorValues[3]>sensorValues[1] && sensorValues[3]>sensorValues[2]){
+        Serial.println("maxpin : A3");
+      }
     }
-    else
-    {
+    else{
       Serial.println("Pressure out of range!");
     }
     delay(1000);
